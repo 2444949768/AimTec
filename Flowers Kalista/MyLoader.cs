27e12b55649639f3f430d4db0290ef553b63424b
NightMoon032 <1076751236@qq.com>
2017-07-22 03:12:56 +0800

    Kalista Done (Not Test)

---
+++ Flowers Kalista/MyLoader.cs
@@ -0,0 +1,24 @@
+﻿namespace Flowers_Kalista
+{
+    #region 
+
+    using Aimtec;
+
+    #endregion
+
+    internal class MyLoader
+    {
+        public static void Main()
+        {
+            Game.OnStart += delegate
+            {
+                if (ObjectManager.GetLocalPlayer().ChampionName != "Kalista")
+                {
+                    return;
+                }
+
+                var KalistaLoader = new MyBase.MyChampions();
+            };
+        }
+    }
+}
