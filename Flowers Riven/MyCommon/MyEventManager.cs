8f3f9876866d94ca3bc675ba236915ed7a62bed6
NightMoon032 <1076751236@qq.com>
2017-07-24 01:22:51 +0800

    Riven Menu

---
+++ Flowers Riven/MyCommon/MyEventManager.cs
@@ -0,0 +1,370 @@
+﻿namespace Flowers_Riven.MyCommon
+{
+    #region
+
+    using Aimtec;
+    using Aimtec.SDK.Damage;
+    using Aimtec.SDK.Damage.JSON;
+    using Aimtec.SDK.Events;
+    using Aimtec.SDK.Extensions;
+    using Aimtec.SDK.Menu.Components;
+    using Aimtec.SDK.Orbwalking;
+    using Aimtec.SDK.TargetSelector;
+    using Aimtec.SDK.Util.Cache;
+
+    using Flowers_Riven.MyBase;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    #endregion
+
+    internal class MyEventManager : MyLogic
+    {
+        private static readonly int _menuX = (int)(Render.Width * 0.91f);
+        private static readonly int _menuY = (int)(Render.Height * 0.04f);
+
+        internal static void Initializer()
+        {
+            try
+            {
+                Game.OnUpdate += OnUpdate;
+                SpellBook.OnCastSpell += OnCastSpell;
+                Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
+                Obj_AI_Base.OnPerformCast += OnPerformCast;
+                Obj_AI_Base.OnPlayAnimation += OnPlayAnimation;
+                Orbwalker.PostAttack += OnPostAttack;
+                Render.OnRender += OnRender;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.Initializer." + ex);
+            }
+        }
+
+        private static void OnUpdate()
+        {
+            try
+            {
+                ResetToDefalut();
+
+                if (Me.IsDead || Me.IsRecalling())
+                {
+                    return;
+                }
+
+                if (FleeMenu["FlowersRiven.FleeMenu.FleeKey"].As<MenuKeyBind>().Enabled && Me.CanMoveMent())
+                {
+                    FleeEvent();
+                }
+
+                KillStealEvent();
+                AutoUseEvent();
+                
+                switch (Orbwalker.Mode)
+                {
+                    case OrbwalkingMode.Combo:
+                        ComboEvent();
+                        break;
+                    case OrbwalkingMode.Mixed:
+                        HarassEvent();
+                        break;
+                    case OrbwalkingMode.Laneclear:
+                        ClearEvent();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnUpdate." + ex);
+            }
+        }
+
+        private static void ResetToDefalut()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.ResetToDefalut." + ex);
+            }
+        }
+
+        private static void FleeEvent()
+        {
+            try
+            {
+                Me.IssueOrder(OrderType.MoveTo, Game.CursorPos);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.FleeEvent." + ex);
+            }
+        }
+
+        private static void KillStealEvent()
+        {
+            try
+            {
+                if (KillStealMenu["FlowersRiven.KillStealMenu.R"].Enabled && R.Ready && isRActive)
+                {
+                    foreach (
+                        var target in
+                        GameObjects.EnemyHeroes.Where(
+                            x =>
+                                x.IsValidTarget(R.Range) &&
+                                KillStealMenu["FlowersRiven.KillStealMenu.RTargetFor" + x.ChampionName].Enabled &&
+                                x.Health < Me.GetSpellDamage(x, SpellSlot.R)))
+                    {
+                        if (target.IsValidTarget(R.Range) && !target.IsUnKillable())
+                        {
+                            R.Cast(target);
+                            return;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.KillStealEvent." + ex);
+            }
+        }
+
+        private static void AutoUseEvent()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.AutoUseEvent." + ex);
+            }
+        }
+
+        private static void ComboEvent()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.ComboEvent." + ex);
+            }
+        }
+
+        private static void BurstEvent(Obj_AI_Hero target)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.BurstEvent." + ex);
+            }
+        }
+
+        private static void HarassEvent()
+        {
+            try
+            {
+                if (Me.IsUnderEnemyTurret())
+                {
+                    return;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.HarassEvent." + ex);
+            }
+        }
+
+        private static void ClearEvent()
+        {
+            try
+            {
+                if (MyManaManager.SpellFarm)
+                {
+                    LaneClearEvent();
+                    JungleClearEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.ClearEvent." + ex);
+            }
+        }
+
+        private static void LaneClearEvent()
+        {
+            try
+            {
+                if (Me.IsUnderEnemyTurret())
+                {
+                    return;
+                }
+
+                var minions = GameObjects.EnemyMinions.Where(x => x.IsValidTarget(Q.Range) && x.Health > 5).ToArray();
+
+                if (minions.Any())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.LaneClearEvent." + ex);
+            }
+        }
+
+        private static void JungleClearEvent()
+        {
+            try
+            {
+                var mobs =
+                    GameObjects.EnemyMinions.Where(
+                        x =>
+                            x.IsValidTarget(Q.Range + Me.AttackRange + Me.BoundingRadius) && x.Health > 5 &&
+                            x.Team == GameObjectTeam.Neutral).ToArray();
+
+                if (mobs.Any())
+                {
+                    var mob = mobs.OrderBy(x => x.MaxHealth).FirstOrDefault(x => x.IsValidTarget(Q.Range));
+
+                    if (mob != null)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.JungleClearEvent." + ex);
+            }
+        }
+
+        private static void OnCastSpell(Obj_AI_Base sender, SpellBookCastSpellEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnCastSpell." + ex);
+            }
+        }
+
+        private static void OnProcessSpellCast(Obj_AI_Base sender, Obj_AI_BaseMissileClientDataEventArgs Args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnProcessSpellCast." + ex);
+            }
+        }
+
+        private static void OnPerformCast(Obj_AI_Base sender, Obj_AI_BaseMissileClientDataEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnPerformCast." + ex);
+            }
+        }
+
+        private static void OnPlayAnimation(Obj_AI_Base sender, Obj_AI_BasePlayAnimationEventArgs Args)
+        {
+            try
+            {
+                if (sender.IsMe)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnPlayAnimation." + ex);
+            }
+        }
+
+        private static void OnPostAttack(object sender, PostAttackEventArgs Args)
+        {
+            try
+            {
+                if (Args.Target == null || Args.Target.IsDead || !Args.Target.IsValidTarget())
+                {
+                    return;
+                }
+
+                switch (Orbwalker.Mode)
+                {
+                    case OrbwalkingMode.Combo:
+
+                        break;
+                    case OrbwalkingMode.Mixed:
+
+                        break;
+                    case OrbwalkingMode.Laneclear:
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnPostAttack." + ex);
+            }
+        }
+
+        private static void OnRender()
+        {
+            try
+            {
+                if (DrawMenu["FlowersRiven.DrawMenu.E"].Enabled && E.Ready)
+                {
+                    Render.Circle(Me.Position, E.Range, 23, Color.FromArgb(0, 136, 255));
+                }
+
+                if (DrawMenu["FlowersRiven.DrawMenu.R"].Enabled && R.Ready)
+                {
+                    Render.Circle(Me.Position, R.Range, 23, Color.FromArgb(251, 0, 133));
+                }
+
+                if (DrawMenu["FlowersRiven.DrawMenu.ComboR"].Enabled)
+                {
+                    Render.Text(_menuX + 10, _menuY + 25, Color.Orange,
+                        "Combo R(" + ComboMenu["FlowersRiven.ComboMenu.R"].As<MenuKeyBind>().Key + "): " +
+                        (ComboMenu["FlowersRiven.ComboMenu.R"].As<MenuKeyBind>().Enabled ? "On" : "Off"));
+                }
+
+                if (DrawMenu["FlowersRiven.DrawMenu.Burst"].Enabled)
+                {
+                    Render.Text(_menuX + 10, _menuY + 45, Color.Orange,
+                        "Burst Combo(" + BurstMenu["FlowersRiven.BurstMenu.Key"].As<MenuKeyBind>().Key + "): " +
+                        (BurstMenu["FlowersRiven.BurstMenu.Key"].As<MenuKeyBind>().Enabled ? "On" : "Off"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyEventManager.OnRender." + ex);
+            }
+        }
+    }
+}
---
+++ Flowers Riven/MyCommon/MyMenuManager.cs
@@ -0,0 +1,224 @@
+﻿using System.Linq;
+using Aimtec;
+
+namespace Flowers_Riven.MyCommon
+{
+    #region 
+
+    using Aimtec.SDK.Menu;
+    using Aimtec.SDK.Menu.Components;
+    using Aimtec.SDK.Util.Cache;
+
+    using Flowers_Riven.MyBase;
+
+    using System;
+
+    #endregion
+
+    internal class MyMenuManager
+    {
+        internal static void Initializer()
+        {
+            try
+            {
+                MyLogic.Menu = new Menu("FlowersRiven", "Flowers Riven", true);
+                {
+                    MyLogic.Menu.Add(new MenuSeperator("CreditName", "Made by NightMoon"));
+                    MyLogic.Menu.Add(new MenuSeperator("IfFPSDrop", "If you have FPS Drop Try F5 reload"));
+                    MyLogic.Menu.Add(new MenuSeperator("ASDASDF"));
+                }
+
+                MyLogic.Orbwalker = new Aimtec.SDK.Orbwalking.Orbwalker();
+                MyLogic.Orbwalker.Attach(MyLogic.Menu);
+
+                MyLogic.ComboMenu =new Menu("FlowersRiven.ComboMenu", ":: Combo Settings");
+                {
+                    MyLogic.ComboMenu.Add(new MenuSeperator("FlowersRiven.ComboMenu.WSettings", "-- W Settings"));
+                    MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.WCancel", "Use W Cancel Animation"));
+
+                    MyLogic.ComboMenu.Add(new MenuSeperator("FlowersRiven.ComboMenu.ESettings", "-- E Settings"));
+                    MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.EGapcloser", "Use E Gapcloser"));
+
+                    MyLogic.ComboMenu.Add(new MenuSeperator("FlowersRiven.ComboMenu.RSettings", "-- R Settings"));
+                    MyLogic.ComboMenu.Add(new MenuKeyBind("FlowersRiven.ComboMenu.R", "Use R", Aimtec.SDK.Util.KeyCode.G,
+                        KeybindType.Toggle, true));
+                    MyLogic.ComboMenu.Add(new MenuList("FlowersRiven.ComboMenu.RMode", "Use R2 Mode: ",
+                        new[] {"My Logic", "Only KillSteal", "First Cast", "Off"}, 0));
+
+                    MyLogic.ComboMenu.Add(new MenuSeperator("FlowersRiven.ComboMenu.RTargetSettings", "-- RTarget Settings"));
+                    foreach (var hero in GameObjects.EnemyHeroes)
+                    {
+                        MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.RTargetFor" + hero.ChampionName,
+                            "Use On " + hero.ChampionName));
+                    }
+
+                    MyLogic.ComboMenu.Add(new MenuSeperator("FlowersRiven.ComboMenu.OtherSettings", "-- Other Settings"));
+                    MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.Ignite", "Use Ignite"));
+                    MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.Item", "Use Item"));
+                    MyLogic.ComboMenu.Add(new MenuBool("FlowersRiven.ComboMenu.Youmuu", "Use Youmuu"));
+                }
+                MyLogic.Menu.Add(MyLogic.ComboMenu);
+
+                MyLogic.BurstMenu = new Menu("FlowersRiven.BurstMenu", ":: Burst Settings");
+                {
+                    MyLogic.BurstMenu.Add(new MenuSeperator("-- How to burst"));
+                    MyLogic.BurstMenu.Add(new MenuSeperator("1.you need to enbaled the Key"));
+                    MyLogic.BurstMenu.Add(new MenuSeperator("2.Select the Target (or not, but this will be force target to burst)"));
+                    MyLogic.BurstMenu.Add(new MenuSeperator("3.and then press the Combo Key"));
+                    MyLogic.BurstMenu.Add(new MenuSeperator("BUSRAWDQWD"));
+                    MyLogic.BurstMenu.Add(new MenuBool("FlowersRiven.BurstMenu.Flash", "Use Flash"));
+                    MyLogic.BurstMenu.Add(new MenuBool("FlowersRiven.BurstMenu.Ignite", "Use Ignite"));
+                    MyLogic.BurstMenu.Add(new MenuList("FlowersRiven.BurstMenu.Mode", "Burst Mode: ",
+                        new[] {"Shy", "EQ3"}, 0));
+                    MyLogic.BurstMenu.Add(new MenuKeyBind("FlowersRiven.BurstMenu.Key", "Burst Active Key(Toggle)",
+                        Aimtec.SDK.Util.KeyCode.T, KeybindType.Toggle));
+                }
+                MyLogic.Menu.Add(MyLogic.BurstMenu);
+
+                MyLogic.HarassMenu = new Menu("FlowersRiven.HarassMenu", ":: Harass Settings");
+                {
+                    MyLogic.HarassMenu.Add(new MenuSeperator("FlowersRiven.HarassMenu.QSettings", "-- Q Settings"));
+                    MyLogic.HarassMenu.Add(new MenuBool("FlowersRiven.HarassMenu.Q", "Use Q"));
+
+                    MyLogic.HarassMenu.Add(new MenuSeperator("FlowersRiven.HarassMenu.WSettings", "-- W Settings"));
+                    MyLogic.HarassMenu.Add(new MenuBool("FlowersRiven.HarassMenu.W", "Use W"));
+
+                    MyLogic.HarassMenu.Add(new MenuSeperator("FlowersRiven.HarassMenu.ESettings", "-- E Settings"));
+                    MyLogic.HarassMenu.Add(new MenuBool("FlowersRiven.HarassMenu.E", "Use E"));
+
+                    MyLogic.HarassMenu.Add(new MenuSeperator("FlowersRiven.HarassMenu.ModeSettings", "-- Mode Settings"));
+                    MyLogic.HarassMenu.Add(new MenuList("FlowersRiven.HarassMenu.Mode", "Harass Mode: ",
+                        new[] {"Smart(will use Q/E return)", "normal"}, 0));
+                }
+                MyLogic.Menu.Add(MyLogic.HarassMenu);
+
+                MyLogic.ClearMenu = new Menu("FlowersRiven.ClearMenu", ":: Clear Settings");
+                {
+                    MyLogic.ClearMenu.Add(new MenuSeperator("FlowersRiven.ClearMenu.LaneClearSettings", "-- LaneClear Settings"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.LaneClearQ", "Use Q"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.LaneClearQSmart", "Use Q| Smart Farm"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.LaneClearQTurret", "Use Q| When Attach Turret reset"));
+                    MyLogic.ClearMenu.Add(new MenuSliderBool("FlowersRiven.ClearMenu.LaneClearW",
+                        "Use W| Min Hit Count >= x", true, 3, 1, 5));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.LaneClearItem", "Use Item"));
+
+                    MyLogic.ClearMenu.Add(new MenuSeperator("FlowersRiven.ClearMenu.JungleClearSettings", "-- JungleClear Settings"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.JungleClearQ", "Use Q"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.JungleClearW", "Use W"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.JungleClearE", "Use E"));
+                    MyLogic.ClearMenu.Add(new MenuBool("FlowersRiven.ClearMenu.JungleClearItem", "Use Item"));
+                }
+                MyLogic.Menu.Add(MyLogic.ClearMenu);
+
+                MyLogic.FleeMenu = new Menu("FlowersRiven.FleeMenu", ":: Flee Settings");
+                {
+                    MyLogic.FleeMenu.Add(new MenuSeperator("FlowersRiven.FleeMenu.KeySettings", "-- Key Settings"));
+                    MyLogic.FleeMenu.Add(new MenuKeyBind("FlowersRiven.FleeMenu.FleeKey", "Flee Key",
+                        Aimtec.SDK.Util.KeyCode.Z, KeybindType.Press));
+
+                    MyLogic.FleeMenu.Add(new MenuSeperator("FlowersRiven.FleeMenu.WSettings", "-- W Settings"));
+                    MyLogic.FleeMenu.Add(new MenuBool("FlowersRiven.FleeMenu.W", "Use W"));
+
+                    MyLogic.FleeMenu.Add(new MenuSeperator("FlowersRiven.FleeMenu.QSettings", "-- Q Settings"));
+                    MyLogic.FleeMenu.Add(new MenuBool("FlowersRiven.FleeMenu.Q", "Use Q"));
+
+                    MyLogic.FleeMenu.Add(new MenuSeperator("FlowersRiven.FleeMenu.ESettings", "-- E Settings"));
+                    MyLogic.FleeMenu.Add(new MenuBool("FlowersRiven.FleeMenu.E", "Use E"));
+                }
+                MyLogic.Menu.Add(MyLogic.FleeMenu);
+
+                MyLogic.KillStealMenu = new Menu("FlowersRiven.KillStealMenu", ":: KillSteal Settings");
+                {
+                    MyLogic.KillStealMenu.Add(new MenuSeperator("FlowersRiven.KillStealMenu.RSettings", "-- R Settings"));
+                    MyLogic.KillStealMenu.Add(new MenuBool("FlowersRiven.KillStealMenu.R", "Use R"));
+
+                    MyLogic.KillStealMenu.Add(new MenuSeperator("FlowersRiven.KillStealMenu.RTargetSettings", "-- RTarget Settings"));
+                    foreach (var hero in GameObjects.EnemyHeroes)
+                    {
+                        MyLogic.KillStealMenu.Add(new MenuBool("FlowersRiven.KillStealMenu.RTargetFor" + hero.ChampionName,
+                            "Use On " + hero.ChampionName));
+                    }
+                }
+                MyLogic.Menu.Add(MyLogic.KillStealMenu);
+
+                MyLogic.MiscMenu = new Menu("FlowersRiven.MiscMenu", ":: Misc Settings");
+                {
+                    MyManaManager.AddFarmToMenu(MyLogic.MiscMenu);
+
+                    MyLogic.MiscMenu.Add(new MenuSeperator("FlowersRiven.MiscMenu.QSettings", "-- Q Settings"));
+                    MyLogic.MiscMenu.Add(new MenuBool("FlowersRiven.MiscMenu.KeepQ", "Keep Q Alive"));
+                    MyLogic.MiscMenu.Add(new MenuList("FlowersRiven.MiscMenu.QMode", "Use Q Mode: ",
+                        new[] {"To target", "To Mouse"}, 0));
+
+                    MyLogic.MiscMenu.Add(new MenuSeperator("FlowersRiven.MiscMenu.AnimationSettings", "-- Animation Settings"));
+                    MyLogic.MiscMenu.Add(new MenuBool("FlowersRiven.MiscMenu.SemiCancel", "Semi Cancel Animation"));
+                    MyLogic.MiscMenu.Add(new MenuBool("FlowersRiven.MiscMenu.CalculatePing", "Calulate your Ping"));
+                    MyLogic.MiscMenu.Add(new MenuSlider("FlowersRiven.MiscMenu.Q1Delay", "Auto R| Min Hit Count >= x", 351, 250, 500));
+                    MyLogic.MiscMenu.Add(new MenuSlider("FlowersRiven.MiscMenu.Q2Delay", "Auto R| My Allies Count >= x", 351, 250, 500));
+                    MyLogic.MiscMenu.Add(new MenuSlider("FlowersRiven.MiscMenu.Q3Delay", "Auto R| My HealthPercednt >= x", 451, 350, 600));
+                }
+                MyLogic.Menu.Add(MyLogic.MiscMenu);
+
+                MyLogic.EvadeMenu = new Menu("FlowersRiven.EvadeMenu", ":: Evade Settings");
+                {
+                    MyLogic.EvadeMenu.Add(new MenuBool("FlowersRiven.EvadeMenu.Use E", "Use E Shield"));
+                    MyLogic.EvadeMenu.Add(new MenuSeperator("ascqwdqwdqqwe"));
+                    if (GameObjects.EnemyHeroes.ToArray().Any())
+                    {
+                        foreach (var target in GameObjects.EnemyHeroes.ToArray())
+                        {
+                            var heroMenu = new Menu(target.ChampionName, target.ChampionName);
+
+                            //TODO
+                            //if (target.SpellBook.GetSpell(SpellSlot.Q).SpellData. == 1)
+                            //{
+                            //    eMenu.AddBool(target.ChampionName + "Skill" + target.Spellbook.GetSpell(SpellSlot.Q).SData.Name,
+                            //        target.SpellBook.GetSpell(SpellSlot.Q).SData.Name);
+                            //}
+
+                            //if (target.SpellBook.GetSpell(SpellSlot.W).SData.TargettingType == SpellDataTargetType.Unit)
+                            //{
+                            //    eMenu.AddBool(target.ChampionName + "Skill" + target.Spellbook.GetSpell(SpellSlot.W).SData.Name,
+                            //        target.SpellBook.GetSpell(SpellSlot.W).SData.Name);
+                            //}
+
+                            //if (target.Spellbook.GetSpell(SpellSlot.E).SData.TargettingType == SpellDataTargetType.Unit)
+                            //{
+                            //    eMenu.AddBool(target.ChampionName + "Skill" + target.Spellbook.GetSpell(SpellSlot.E).SData.Name,
+                            //        target.Spellbook.GetSpell(SpellSlot.E).SData.Name);
+                            //}
+
+                            //if (target.Spellbook.GetSpell(SpellSlot.R).SData.TargettingType == SpellDataTargetType.Unit)
+                            //{
+                            //    eMenu.AddBool(target.ChampionName + "Skill" + target.Spellbook.GetSpell(SpellSlot.R).SData.Name,
+                            //        target.Spellbook.GetSpell(SpellSlot.R).SData.Name);
+                            //}
+                        }
+                    }
+                }
+                MyLogic.Menu.Add(MyLogic.EvadeMenu);
+
+                MyLogic.DrawMenu = new Menu("FlowersRiven.DrawMenu", ":: Draw Settings");
+                {
+                    MyLogic.DrawMenu.Add(new MenuSeperator("FlowersRiven.DrawMenu.RangeSettings", "-- Spell Range"));
+                    MyLogic.DrawMenu.Add(new MenuBool("FlowersRiven.DrawMenu.W", "Draw W Range", false));
+                    MyLogic.DrawMenu.Add(new MenuBool("FlowersRiven.DrawMenu.E", "Draw E Range", false));
+                    MyLogic.DrawMenu.Add(new MenuBool("FlowersRiven.DrawMenu.R", "Draw R Range", false));
+
+                    MyLogic.DrawMenu.Add(new MenuSeperator("FlowersRiven.DrawMenu.StatusSettings", "-- Logic Status"));
+                    MyLogic.DrawMenu.Add(new MenuBool("FlowersRiven.DrawMenu.ComboR", "Draw Combo R Status", false));
+                    MyLogic.DrawMenu.Add(new MenuBool("FlowersRiven.DrawMenu.Burst", "Draw Burst Status", false));
+
+                    MyManaManager.AddDrawToMenu(MyLogic.DrawMenu);
+                }
+                MyLogic.Menu.Add(MyLogic.DrawMenu);
+
+                MyLogic.Menu.Attach();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in MyMenuManager.Initializer." + ex);
+            }
+        }
+    }
+}
